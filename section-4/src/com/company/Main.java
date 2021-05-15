8aeda5b3fa362c4834ef5c0d264a280b4f7912fb
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-14 23:30:47 -0300

    section-4-ex2

--- section-4/src/com/company/Main.java
+++ section-4/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import static com.company.ex1_SpeedConverter.printConversion;
 import static com.company.ex1_SpeedConverter.toMilesPerHour;
+import static com.company.ex2_MegaBytesConverter.printMegaBytesAndKiloBytes;
 
 public class Main {
 
@@ -19,6 +20,12 @@ public class Main {
         printConversion(-5.6); // should print the following text (into the console - System.out): Invalid Value
         printConversion(25.42); // should print the following text (into the console - System.out): 25.42 km/h = 16 mi/h
         printConversion(75.114); // should print the following text (into the console - System.out): 75.114 km/h = 47 mi/h
+*/
+        // Exercise 2 - MegaBytes Converter
+/*
+        printMegaBytesAndKiloBytes(2500); // should print the following text: "2500 KB = 2 MB and 452 KB"
+        printMegaBytesAndKiloBytes(-1024); // should print the following text: "Invalid Value" because parameter is less than 0.
+        printMegaBytesAndKiloBytes(5000); // should print the following text: "5000 KB = 4 MB and 904 KB"
 */
     }
 }
---
+++ section-4/src/com/company/ex2_MegaBytesConverter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ex2_MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        String message = "Invalid Value";
+
+        if (kiloBytes >= 0) {
+            message = String.format("%d KB = %d MB and %d KB", kiloBytes, kiloBytes / 1024, kiloBytes % 1024);
+        }
+
+        System.out.println(message);
+    }
+}
