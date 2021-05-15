465eaad3b4b16f7f7e2fcf6cca0173a168a32b73
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-14 23:13:41 -0300

    section-4-ex1

---
+++ section-4/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import static com.company.ex1_SpeedConverter.printConversion;
+import static com.company.ex1_SpeedConverter.toMilesPerHour;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Exercise 1 - Speed Converter
+/*
+        System.out.println(toMilesPerHour(1.5)); //should return value 1
+        System.out.println(toMilesPerHour(10.25)); //should return value 6
+        System.out.println(toMilesPerHour(-5.6)); //should return value -1
+        System.out.println(toMilesPerHour(25.42)); //should return value 16
+        System.out.println(toMilesPerHour(75.114)); //should return value 47
+
+        printConversion(1.5); // should print the following text (into the console - System.out): 1.5 km/h = 1 mi/h
+        printConversion(10.25); // should print the following text (into the console - System.out): 10.25 km/h = 6 mi/h
+        printConversion(-5.6); // should print the following text (into the console - System.out): Invalid Value
+        printConversion(25.42); // should print the following text (into the console - System.out): 25.42 km/h = 16 mi/h
+        printConversion(75.114); // should print the following text (into the console - System.out): 75.114 km/h = 47 mi/h
+*/
+    }
+}
