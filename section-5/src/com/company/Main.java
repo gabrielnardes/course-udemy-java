9a5defd15c849afd13e3ad40fbfbb7326926efb9
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-15 21:08:42 -0300

    section-5-ex12

---
+++ section-5/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import static com.company.ex12_NumberInWord.printNumberInWord;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Exercise 12 - Number In Word
+/*
+        printNumberInWord(1);
+        printNumberInWord(5);
+        printNumberInWord(1123);
+        printNumberInWord(-1);
+*/
+    }
+}
