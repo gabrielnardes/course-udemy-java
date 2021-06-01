bc912a3e5e1bfae22565583b27158ba78f058aac
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-06-01 17:17:50 -0300

    Separate examples in methods for section 5

--- section5/Section5.java
+++ section5/Section5.java
@@ -1,41 +1,56 @@
 package section5;
 
+import static section5.ex12_NumberInWord.printNumberInWord;
+import static section5.ex13_NumberOfDaysInMonth.getDaysInMonth;
+import static section5.ex14_SumOddRange.sumOdd;
+import static section5.ex15_NumberPalindrome.isPalindrome;
 import static section5.ex16_FirstAndLastDigitSum.sumFirstAndLastDigit;
 
 public class Section5 {
     public static void main(String[] args) {
-        // Exercise 12 - Number In Word
-/*
-        printNumberInWord(1);
-        printNumberInWord(5);
-        printNumberInWord(1123);
-        printNumberInWord(-1);
-*/
-        // Exercise 13 - Exercise 13: Number Of Days In Month
-/*
+        ex12();
+        ex13();
+        ex14();
+        ex15();
+        ex16();
+    }
+
+    private static void ex12() {
+        System.out.println("\nExercise 12 - Number In Word - Section 5");
+        printNumberInWord(1); // should print ONE
+        printNumberInWord(5); // should print FIVE
+        printNumberInWord(1123); // should print OTHER
+        printNumberInWord(-1); // should print OTHER
+    }
+
+    private static void ex13() {
+        System.out.println("\nExercise 13 - Number Of Days In Month - Section 5");
         System.out.println(getDaysInMonth(1, 2020)); // should return 31 since January has 31 days.
         System.out.println(getDaysInMonth(2, 2020)); // should return 29 since February has 29 days in a leap year and 2020 is a leap year.
         System.out.println(getDaysInMonth(2, 2018)); // should return 28 since February has 28 days if it's not a leap year and 2018 is not a leap year.
         System.out.println(getDaysInMonth(-1, 2020)); // should return -1 since the parameter month is invalid.
         System.out.println(getDaysInMonth(1, -2020)); // should return -1 since the parameter year is outside the range of 1 to 9999.
-*/
-        // Exercise 14 - Sum Odd
- /*
+    }
+
+    private static void ex14() {
+        System.out.println("\nExercise 14 - Sum Odd - Section 5");
         System.out.println(sumOdd(1, 100)); // should return 2500
         System.out.println(sumOdd(-1, 100)); // should return -1
         System.out.println(sumOdd(100, 100)); // should return 0
         System.out.println(sumOdd(13, 13)); // should return 13 (This set contains one number, 13, and it is odd)
         System.out.println(sumOdd(100, -100)); // should return -1
         System.out.println(sumOdd(100, 1000)); // should return 247500
- */
-        // 15 - Number Palindrome
-/*
+    }
+
+    private static void ex15() {
+        System.out.println("\nExercise 15 - Number Palindrome - Section 5");
         System.out.println(isPalindrome(-1221)); // should return true
         System.out.println(isPalindrome(707)); // should return true
         System.out.println(isPalindrome(11212)); // should return false because reverse is 21211 and that is not equal to 11212.
-*/
+    }
 
-        // Exercise 16 - First And Last Digit Sum
+    private static void ex16() {
+        System.out.println("\nExercise 16 - First And Last Digit Sum - Section 5");
         System.out.println(sumFirstAndLastDigit(252)); // should return 4, the first digit is 2 and the last is 2 which gives us 2+2 and the sum is 4.
         System.out.println(sumFirstAndLastDigit(257)); // should return 9, the first digit is 2 and the last is 7 which gives us 2+7 and the sum is 9.
         System.out.println(sumFirstAndLastDigit(0)); // should return 0, the first digit and the last digit is 0 since we only have 1 digit, which gives us 0+0 and the sum is 0.
